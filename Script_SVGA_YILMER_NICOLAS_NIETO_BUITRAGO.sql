4d3fd21b758cd29bc33ee86b2231446bd58a65d7
Yilmer <yilmernicolasnietobuitrago@gmail.com>
2026-04-06 16:27:06 +0000

    Script

--- Script_SVGA_YILMER_NICOLAS_NIETO_BUITRAGO.sql
+++ Script_SVGA_YILMER_NICOLAS_NIETO_BUITRAGO.sql
@@ -2,11 +2,14 @@ CREATE DATABASE Trazabilidad_SVGA;
 USE Trazabilidad_SVGA;
 
 CREATE TABLE empresa (
-    id_empresa INT AUTO_INCREMENT PRIMARY KEY,
-    codigo_empresa VARCHAR(50) NOT NULL UNIQUE,
+    codigo_empresa VARCHAR(50),
     nombre_empresa VARCHAR(100) NOT NULL
 );
-CREATE TABLE trazabilidada (
+
+ALTER TABLE empresa
+ADD PRIMARY KEY (codigo_empresa);
+
+CREATE TABLE trazabilidad (
     id_trazabilidad INT AUTO_INCREMENT PRIMARY KEY,
     nombre_empresa VARCHAR(100),
     encargado VARCHAR(100),
@@ -16,5 +19,11 @@ CREATE TABLE trazabilidada (
     correo VARCHAR(100),
     estado VARCHAR(50),
     fecha_cierre DATE NOT NULL,
-    id_empresa INT
+    codigo_empresa VARCHAR(50),
+
+    CONSTRAINT fk_empresa 
+    FOREIGN KEY (codigo_empresa)
+    REFERENCES empresa(codigo_empresa)
 );
+insert into empresa
+values (
